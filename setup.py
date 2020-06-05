ecfdff9a8d0b6367afbf988d395eb09563d4e154
Nico Tomeo <nico@canotic.com>
2020-06-05 15:57:46 +0200

    Updates setup.py to publish in pypi.
    Adds README description and updates url, author and email.

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@
 
 from setuptools import setup, find_packages
 
-NAME = "superai_api_client"
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+NAME = "superai"
 VERSION = "0.0.1"
 # To install the library, run the following
 #
@@ -23,15 +26,15 @@ setup(
     name=NAME,
     version=VERSION,
     description="super.AI API",
-    author_email="",
-    url="https://super.ai/",
+    author='super.AI',
+    author_email="support@super.ai",
+    url="https://github.com/mysuperai/superai-api-client",
     keywords=["super.AI API"],
     install_requires=REQUIRES,
     packages=find_packages(),
     include_package_data=True,
-    long_description="""\
-    super.AI App Store API  
-    """,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     entry_points={
         'console_scripts': [
             'superai=superai.cli:main',
