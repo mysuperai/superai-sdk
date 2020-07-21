52162679a68aefd041c9d290fdcfe20d6266249e
Brad Cordova <550797+bcordo@users.noreply.github.com>
2020-07-21 10:26:35 +0200

    fix instance creation input

--- superai/apis/dp_instance.py
+++ superai/apis/dp_instance.py
@@ -1,17 +1,18 @@
-from typing import List, Generator, Dict
+from typing import List, Generator, Dict, Union
+import names
 
 from superai.apis.data_program_base import DataProgramBase
 
 
 class DataProgramInstance(DataProgramBase):
-    def __init__(self, template_id: str, quality=None, cost=None, latency=None, name=None, description=None, **kwargs):
+    def __init__(self, template_id: Union[int, float], quality=None, cost=None, latency=None, name=None, description=None, **kwargs):
         self.template_id = template_id
         self.quality = quality
         self.cost = cost
         self.latency = latency
         self.__dict__.update(kwargs)
 
-        self.__instance_object = self.__create_instance(parameters=kwargs, performance={
+        self.__instance_object = self.__create_instance(template_id=template_id, parameters=kwargs, performance={
             "quality": quality,
             "cost": cost,
             "latency": latency
@@ -20,7 +21,7 @@ class DataProgramInstance(DataProgramBase):
         assert "id" in self.__instance_object
         self.instance_id = self.__instance_object["id"]
 
-    def __create_instance(self, parameters: Dict=None, performance: Dict=None, name: str = None, description: str = None) -> Dict:
+    def __create_instance(self, template_id:Union[int, float], parameters: Dict=None, performance: Dict=None, name: str = None, description: str = None) -> Dict:
         """
         Create a data program instance
         :param parameters:
@@ -34,11 +35,11 @@ class DataProgramInstance(DataProgramBase):
             body_json['parameters'] = parameters
         if performance is not None:
             body_json['performance'] = performance
-        if name is not None:
-            body_json['name'] = name
+        if name is None:
+            body_json['name'] = names.get_first_name()
         if description is not None:
             body_json['description'] = description
-        uri = f'template/{self.template_id}/instance'
+        uri = f'template/{template_id}/instance'
         return self._request(uri, method='POST', body_params=body_json, required_api_key=False)
 
     def label(self, inputs: List[Dict]) -> Dict:
@@ -48,6 +49,7 @@ class DataProgramInstance(DataProgramBase):
         """
         body_json = {
             "inputs": inputs,
+            "job_type": "normal"
         }
         if self.quality is not None:
             body_json['quality'] = self.quality
