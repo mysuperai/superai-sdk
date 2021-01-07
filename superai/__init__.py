f3f7feab9a8ad28800f09333600c5b5fd7e4b592
superai-cicd <ci@super.ai>
2021-01-07 18:13:39 +0000

    Jenkins Build ID 33 new version 0.0.6.build3

--- superai/__init__.py
+++ superai/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 
-__version__ = '0.0.6.build2'
+__version__ = '0.0.6.build3'
 
 from .client import *
 from superai.config import settings
