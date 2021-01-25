f35a95626ba86a36ad8741c219a9ca55a64d1787
Nico Tomeo <nico@canotic.com>
2021-01-25 14:12:12 -0300

    Updates version

--- superai/__init__.py
+++ superai/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-__version__ = "0.0.7"
+__version__ = "0.0.8"
 
 from superai.config import settings
 from .client import *
