e29239038d8a879a5890096663d8508a09a004cf
superai-cicd <ci@super.ai>
2021-01-27 13:29:54 +0000

    Jenkins Build ID 47 new version 0.1.0.alpha1.build4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 NAME = "superai"
-VERSION = "0.1.0.alpha1.build3"
+VERSION = "0.1.0.alpha1.build4"
 # To install the library, run the following
 #
 # python setup.py install
