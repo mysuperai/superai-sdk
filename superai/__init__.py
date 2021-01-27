994a299c72c4215beb8fc6e1d315b5628b5365fc
superai-cicd <ci@super.ai>
2021-01-27 17:13:07 +0000

    Jenkins Build ID 51 new version 0.1.0.alpha1.build6

--- superai/__init__.py
+++ superai/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-__version__ = "0.1.0.alpha1.build5"
+__version__ = "0.1.0.alpha1.build6"
 
 # Client comes first
 from .client import *
