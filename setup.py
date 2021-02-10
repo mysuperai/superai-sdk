cfd504ec1e03ea403110c242141a8e66975af1fe
Enrique Garcia <enrique@super.ai>
2021-02-10 21:25:52 +0100

    Going back to v0.1.0.a0.dev1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 NAME = "superai"
-VERSION = "0.1.0.alpha1.dev22"
+VERSION = "0.1.0.alpha0.dev1"
 # To install the library, run the following
 #
 # python setup.py install
