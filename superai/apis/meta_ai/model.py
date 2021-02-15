5ab49029291887cb5ff51b824712b4f92450c305
Lukas Pfannschmidt <lukas@super.ai>
2021-02-15 20:21:37 +0100

    Add tests for model creation and retrieval

--- superai/apis/meta_ai/model.py
+++ superai/apis/meta_ai/model.py
@@ -15,10 +15,8 @@ from superai.apis.meta_ai.meta_ai_schema import (
     meta_ai_model_pk_columns_input,
     meta_ai_app_insert_input,
     meta_ai_app_constraint,
-    meta_ai_visibility_enum
+    meta_ai_visibility_enum,
 )
-
-app_id = "f771605e-7ae6-4431-82f6-4d5651226f44"
 
 
 class MetaAISession(RequestsEndpoint):
@@ -56,8 +54,12 @@ def add_model(
     op = Operation(mutation_root)
     op.insert_meta_ai_model_one(
         object=meta_ai_model_insert_input(
-            name=name, description=description, version=version, metadata=metadata, endpoint=endpoint,
-            visibility=visibility
+            name=name,
+            description=description,
+            version=version,
+            metadata=metadata,
+            endpoint=endpoint,
+            visibility=visibility,
         )
     ).__fields__("name", "version", "id", "endpoint")
     data = sess(op)
@@ -115,61 +117,3 @@ def deactivate_app(app_id):
     if output == app_id:
         logger.info(f"Deactivated model for app_id: {app_id}")
         return output
-
-
-if __name__ == "__main__":
-    # sess = MetaAISession()
-    # m_id = "8220ada2-24fc-4466-85ca-1bbf757d6f92"
-    other_m = "bd90c609-eb6a-4d51-aa34-06c3dab6917c"
-    # m = get_model(m_id)
-    # print(m)
-    # m = get_all_models()
-    # print(m)
-
-    # m = get_app("f771605e-7ae6-4431-82f6-4d5651226f44")
-    # print(m)
-    # a = get_apps()
-    # print(a)
-
-    a = add_model("AddedModel")
-    print(a)
-    # b = update_model(a, name="ChangedModel", version=2)
-    # print(b)
-    c = delete_model(a)
-    print(c)
-    #new_app = "ef83890f-baf7-407d-8ecc-1e5676c089a9"
-    #active = get_active_model(new_app)
-    #print(active)
-    #active = set_active_model(new_app, other_m)
-    #print(active)
-    #active = deactivate_app(new_app)
-    #active = set_active_model(new_app, other_m)
-    #print(active)
-
-    # new_active = upsert_app_mapping(app_id, other_m)
-    # print(new_active)
-    # new_active = upsert_app_mapping(app_id, m_id)
-    # op = Operation(query_root)
-    # op.meta_ai_model_by_pk(id=m_id)
-    # data = sess(op)
-    # print(data)
-    # print(op+data)
-    # preds = sess.get_predictions("8220ada2-24fc-4466-85ca-1bbf757d6f92", 123)
-    # #print(list(preds))
-    # print(sess)
-
-    # op = Operation(Query,name="GetModel")
-    # print(op.model(id=m_id))
-    # #name = op.model()
-    # #print(name)
-    # data = sess(op)
-    # print(data)
-
-    # obj = op+data
-    # print(obj)
-
-    # op2 = Operation(Mutation, name="AddModel")
-    # add = op2.addModel(name="Test2", version=1,endpoint="localhost")
-    # print(add)
-    # added = sess(op2)
-    # print(added)
---
+++ tests/apis/test_meta_ai.py
@@ -0,0 +1,30 @@
+from superai.apis.meta_ai import model as api
+import uuid
+import pytest
+
+def test_model_creation():
+    id = uuid.uuid4()
+    a = api.add_model(f"TestModel-{id}")
+    assert a is not None
+    b = api.update_model(a, name=f"ChangedTestModel-{id}", version=2)
+    assert a == b
+    c = api.delete_model(a)
+    assert a == c
+
+
+@pytest.fixture()
+def model():
+    id = uuid.uuid4()
+    a = api.add_model(f"TestModel-{id}")
+    assert a is not None
+    yield a
+    c = api.delete_model(a)
+    assert a == c
+
+
+def test_model_retrieval(model):
+    m = api.get_model(model)
+    assert "name" in m
+
+    m = api.get_all_models()
+    assert len(m) >= 1
