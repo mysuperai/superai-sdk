ec1e0748ea1e70ce47b057a92002273a319f5ed2
Lukas Pfannschmidt <lukas@super.ai>
2021-02-16 16:02:49 +0100

    Add new model fields

--- superai/apis/meta_ai/model.py
+++ superai/apis/meta_ai/model.py
@@ -9,7 +9,7 @@ log = logger.get_logger(__name__)
 
 from superai.apis.meta_ai.meta_ai_schema import (
     meta_ai_app_constraint, meta_ai_app_insert_input, meta_ai_app_on_conflict,
-    meta_ai_app_update_column, meta_ai_model_insert_input,
+    meta_ai_app_update_column, meta_ai_assignment_enum, meta_ai_model_insert_input,
     meta_ai_model_pk_columns_input, meta_ai_model_set_input,
     meta_ai_visibility_enum, mutation_root, query_root)
 
@@ -61,10 +61,11 @@ def add_model(
             version=version,
             metadata=metadata,
             endpoint=endpoint,
-            visibility=visibility,
+            visibility=visibility
         )
-    ).__fields__("name", "version", "id", "endpoint")
+    ).__fields__("name", "version", "id", "endpoint", "description")
     data = sess.perform_op(op)
+    logger.info(f"Created new model: {data}")
     return (op + data).insert_meta_ai_model_one.id
 
 
@@ -73,7 +74,7 @@ def update_model(id, **kwargs):
     op = Operation(mutation_root)
     op.update_meta_ai_model_by_pk(
         _set=meta_ai_model_set_input(**kwargs), pk_columns=meta_ai_model_pk_columns_input(id=id)
-    ).__fields__("name", "version", "id", "endpoint")
+    ).__fields__("name", "version", "id", "endpoint", "description")
     data = sess.perform_op(op)
     return (op + data).update_meta_ai_model_by_pk.id
 
@@ -86,13 +87,13 @@ def delete_model(id):
     return (op + data).delete_meta_ai_model_by_pk.id
 
 
-def get_active_model(app_id):
+def get_active_model(app_id, assignment:meta_ai_assignment_enum):
     sess = MetaAISession(app_id=app_id)
     op = Operation(query_root)
-    op.meta_ai_app_by_pk(id=app_id).model.__fields__("name", "version", "id", "endpoint")
+    op.meta_ai_app(id=app_id, assigned=assignment, active=True).model.__fields__("name", "version", "id", "endpoint", "description", "active")
     data = sess.perform_op(op)
     try:
-        output = (op + data).meta_ai_app_by_pk.model
+        output = (op + data).meta_ai_app.model
         return output
     except AttributeError as e:
         logger.info(f"No active model for app_id: {app_id}")
