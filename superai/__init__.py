ea19850a6306b7348f8673c6e1acf81a5062dc21
Nico Tomeo <nico@canotic.com>
2021-02-17 15:35:32 +0100

    Manual bump version 0.0.9

--- superai/__init__.py
+++ superai/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-__version__ = "0.0.8"
+__version__ = "0.0.9"
 
 from superai.config import settings
 from .client import *
