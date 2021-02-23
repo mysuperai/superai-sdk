50f0adffa81e6710d292531e1e0912ded39e4508
superai-cicd <ci@super.ai>
2021-02-23 11:52:23 +0000

    Add video-bounding-box-keypoint example (#24)
    * Add a basic example for a video-bounding-box-keypoint DP
    
    * Adding max_attempts to task
    
    * Fix input list comprenhension
    
    * Setting collaborator as default labeler on app creation
    
    * Removing max_retries on Task interface in favor of max_attemps
    
    * Adding download data api and enhancing documentation of get_signed_url
    
    * Formatting with black
    
    * Removing fix app uuid
    
    * Adding utils functions to deal with urls that can handle data urls
    
    Co-authored-by: Enrique Garcia <enrique@super.ai> (+1 squashed commit)
    Squashed commits:
    [af13354] Jenkins Build ID 108 new version 0.1.0.alpha1.dev4

--- superai/data_program/utils.py
+++ superai/data_program/utils.py
@@ -3,8 +3,12 @@ from functools import wraps
 from inspect import getframeinfo, stack
 from typing import Callable
 
+import requests
+
+from superai import Client
 from superai.data_program.protocol.task import _parse_args
 from superai.log import logger
+from superai.utils import load_api_key
 
 
 def parse_dp_definition(dp_definition):
@@ -12,6 +16,44 @@ def parse_dp_definition(dp_definition):
     output_schema = _parse_args(schema=dp_definition.get("output_schema")).get("schema")
     parameter_schema = _parse_args(schema=dp_definition.get("parameter_schema")).get("schema")
     return input_schema, output_schema, parameter_schema
+
+
+def sign_url(url: str, client: Client = None):
+    """
+    Get signed url for a dataset given the resource URL. If the path is not a proper data path returns an unsigned URL
+    in the response object
+
+    :param url: Request URL
+    :param client: :class:`Client <superai.client.Client>`
+    :return: Signed url
+    """
+    if url.startswith("data://"):
+        client = client if client else Client(api_key=load_api_key())
+        return client.get_signed_url(path=url).get("signedUrl")
+
+    return url
+
+
+def download_content(url: str, client: Client = None, timeout: int = 10):
+    """
+    Downloads data given a `"data://..."` or URL path.
+
+    :param url: Dataset's path or URL. If the URL is a `data` path then a signed URL will be generated first. If a
+                    standard URL is passed then the `requests` library is used to load the URL and return the content
+                    using response.json()
+    :param client: :class:`Client <superai.client.Client>`
+    :param timeout: (optional) How many seconds to wait for the server to send data before giving up, as a float.
+    :return: URL content
+    """
+    if url.startswith("data://"):
+        client = client if client else Client(api_key=load_api_key())
+        return client.download_data(url, timeout=timeout)
+
+    res = requests.get(url, timeout=timeout)
+    if res.status_code == 200:
+        return res.json()
+    else:
+        raise Exception(res.reason)
 
 
 def IgnoreInAgent(fn: Callable):
