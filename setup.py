c8fdded6f0a272fa4e68c55cee978e34feb5e3d5
Lukas Pfannschmidt <lukas@super.ai>
2021-04-22 18:02:47 +0200

    fix/ecs_logging (#37)
    * fix(logging): use old logger in ECS and Jenkins to prevent linebreaks
    
    * Bump version: 0.1.0.alpha4.dev1 → 0.1.0.alpha5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 NAME = "superai"
-VERSION = "0.1.0.alpha4.dev1"
+VERSION = "0.1.0.alpha5"
 # To install the library, run the following
 #
 # python setup.py install
