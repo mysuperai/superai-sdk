ccd3880291f02020c51783f6a9c18b5bcf255c51
Enrique Garcia <enrique@super.ai>
2021-05-20 22:41:18 +0200

    Releasing alpha 3 version

--- superai/__init__.py
+++ superai/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-__version__ = "0.1.0.alpha2"
+__version__ = "0.1.0.alpha3"
 
 # Client comes first
 from .client import *
