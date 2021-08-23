2f304b51a6c33db51960b1db6ce8df3491e357a3
Jenkins <ci@canotic.com>
2021-08-23 11:05:01 +0000

    fix: login to ECR before pulling the base image (#74) (+1 squashed commit) Squashed commits: [44b4ba7] Jenkins Build ID 65 new version 0.1.0.beta2.dev2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 NAME = "superai"
-VERSION = "0.1.0.beta2.dev1"
+VERSION = "0.1.0.beta2.dev2"
 # To install the library, run the following
 #
 # python setup.py install
