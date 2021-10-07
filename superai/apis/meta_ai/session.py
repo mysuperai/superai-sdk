5f164b8bba572f3074ecc122ab9212d5c1b7805f
Lukas Pfannschmidt <lukas@super.ai>
2021-10-07 13:15:08 +0200

    fix(API): extend timeout to 60 seconds and add retry, fixes DS-SDK-7Z9 (#98)

--- superai/apis/meta_ai/session.py
+++ superai/apis/meta_ai/session.py
@@ -2,6 +2,7 @@ from sgqlc.endpoint.requests import RequestsEndpoint
 from sgqlc.operation import Operation
 from superai.config import settings
 from superai.utils.apikey_manager import load_api_key
+from superai.utils.decorators import retry
 
 
 class GraphQlException(Exception):
@@ -9,16 +10,21 @@ class GraphQlException(Exception):
 
 
 class MetaAISession(RequestsEndpoint):
-    def __init__(self, app_id: str = None, timeout: int = 20):
+    def __init__(self, app_id: str = None, timeout: int = 60):
         base_url = settings.get("meta_ai_base")
         # base_url = "http://localhost:52619/v1/graphql"
         api_key = load_api_key()
         headers = {"x-api-key": api_key, "x-app-id": app_id, "Accept-Encoding": "gzip"}
         super().__init__(base_url, headers, timeout=timeout)
 
-    def perform_op(self, op: Operation, timeout: int = 20):
+    @retry(TimeoutError)
+    def perform_op(self, op: Operation, timeout: int = 60):
         data = self(op, timeout=timeout)
         if data.get("errors", False):
-            raise GraphQlException(data)
+            error = data["errors"][0]
+            if "Endpoint request timed out" in str(error):
+                raise TimeoutError()
+            else:
+                raise GraphQlException(data)
         else:
             return data
