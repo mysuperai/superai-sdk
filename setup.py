40896afff7778ea1eb1fc23319167abae6dcc240
Lukas Pfannschmidt <lukas@super.ai>
2021-10-26 15:36:32 +0200

    fix: replace warrant with pycognito (#106)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 NAME = "superai"
-VERSION = "0.1.0.beta3.dev17"
+VERSION = "0.1.0.beta3.dev18"
 # To install the library, run the following
 #
 # python setup.py install
@@ -39,7 +39,7 @@ REQUIRES = [
     "sgqlc>=14.1",
     "sentry-sdk>=0.19.4",
     "six",
-    "warrant>=0.6",
+    "pycognito>=2021.3.1",
     "psutil~=5.6.7",
 ]
 
