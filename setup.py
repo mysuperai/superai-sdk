aa9c4fd9dc171b27712b63f91b2e6d141e1483af
Jenkins <ci@canotic.com>
2021-11-10 17:03:29 +0000

    Jenkins Build ID 119 new version 0.1.0.beta3.dev23

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 NAME = "superai"
-VERSION = "0.1.0.beta3.dev22"
+VERSION = "0.1.0.beta3.dev23"
 # To install the library, run the following
 #
 # python setup.py install
