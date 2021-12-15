1b94f58ab291fd4a1b6d9c583ad9b675a5d97da6
Enrique Garcia <enrique@super.ai>
2021-12-15 01:02:56 +0100

    fix: bug in workflow update call (#126)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 NAME = "superai"
-VERSION = "0.1.0.beta3.dev34"
+VERSION = "0.1.0.beta3.dev35"
 # To install the library, run the following
 #
 # python setup.py install
--- superai/__init__.py
+++ superai/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-__version__ = "0.1.0.beta3.dev34"
+__version__ = "0.1.0.beta3.dev35"
 
 # Client comes first
 from .client import *
