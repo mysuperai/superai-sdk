fe8c1ae42b3743395dec80de791de1107fb86069
Ankit Mishra <ankit@canotic.com>
2022-01-03 15:07:40 +0100

    fix: passing kubernetes config to k8s deployment (#138)
    * fix: passing kubernetes config
    
    * fix: example
    
    * fix: reading kubernetes_config as well as keywords

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 NAME = "superai"
-VERSION = "0.1.0.beta3.dev44"
+VERSION = "0.1.0.beta3.dev45"
 # To install the library, run the following
 #
 # python setup.py install
