bc5e993bfe252d7dbeecfeedd70fc8443c88ff24
Jenkins <ci@canotic.com>
2022-01-03 14:11:40 +0000

    Jenkins Build ID 165 new version 0.1.0.beta3.dev45

--- superai/__init__.py
+++ superai/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-__version__ = "0.1.0.beta3.dev44"
+__version__ = "0.1.0.beta3.dev45"
 
 # Client comes first
 from .client import *
