9b2b50ae0dd587aaa75bd4e404b4a4ccc8079a60
Jenkins <ci@canotic.com>
2022-01-12 16:58:26 +0000

    Jenkins Build ID 178 new version 0.1.0.beta3.dev51

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 NAME = "superai"
-VERSION = "0.1.0.beta3.dev50"
+VERSION = "0.1.0.beta3.dev51"
 # To install the library, run the following
 #
 # python setup.py install
--- superai/__init__.py
+++ superai/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-__version__ = "0.1.0.beta3.dev50"
+__version__ = "0.1.0.beta3.dev51"
 
 # Client comes first
 from .client import *
