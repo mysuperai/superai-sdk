18e4182c72b4cd77b805c412c617ff18739eb3f2
Ankit Mishra <ankit@super.ai>
2022-01-14 16:10:20 +0100

    fix(version): Updated version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 NAME = "superai"
 
-VERSION = "0.1.0.beta4.dev4"
+VERSION = "0.1.0.beta4"
 # To install the library, run the following
 #
 # python setup.py install
