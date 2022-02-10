78603b58e0dca6d9e9113eea9f2171151137d3bc
Ankit Mishra <ankit@super.ai>
2022-02-10 16:19:13 +0100

    fix(s2i): enable failing the cli command when there is an error code (#169)
    * feat(s2i): enable failing the cli command when there is an error code
    
    * feat(s2i): added test
    
    * fix: getting account name and region from shell
    
    * style: lint

--- superai/__init__.py
+++ superai/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 
-__version__ = "0.1.0.beta5.dev3"
+__version__ = "0.1.0.beta5.dev4"
 
 # Client comes first
 from .client import *
