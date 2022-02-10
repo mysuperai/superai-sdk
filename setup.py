f009c40e0d0ad4b8cc800008568cae453bdd7c86
Ankit Mishra <ankit@super.ai>
2022-02-10 16:19:13 +0100

    fix(s2i): enable failing the cli command when there is an error code (#169)
    * feat(s2i): enable failing the cli command when there is an error code
    
    * feat(s2i): added test
    
    * fix: getting account name and region from shell
    
    * style: lint

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 NAME = "superai"
 
-VERSION = "0.1.0.beta5.dev3"
+VERSION = "0.1.0.beta5.dev4"
 # To install the library, run the following
 #
 # python setup.py install
