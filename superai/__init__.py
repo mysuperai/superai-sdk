9c46720ce0c838029358c2a9827093b863693690
Luis Santos <lsantos.dev@gmail.com>
2022-02-24 13:54:51 +0100

    Add post process endpoint to DPServer (#170)
    * adding HandlerOutput as return type of handler
    
    * updating data_program.py with HandlerOutput
    
    * adding post_process endpoint in DPServer
    
    * Update test_schema_server with HandlerOutput
    
    * adding PostProcessedRequestModel to request; returning 204 No Content if post_process_fn not provided
    
    * creating PostProcessContext class to pass to post_process function
    
    * logging exceptions of failed function calls in DP Server
    
    * added `use_job_cache` param to make it clear when it's being used

--- superai/__init__.py
+++ superai/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 
-__version__ = "0.1.0.beta5.dev9"
+__version__ = "0.1.0.beta5.dev10"
 
 # Client comes first
 from .client import *
