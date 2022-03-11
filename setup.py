c820e9b0212a6103753600547e404432f49bd59c
Lukas Pfannschmidt <lukas@super.ai>
2022-03-11 18:55:09 +0100

    fix(wait): if exception during waiting occured, the model served_by field was never updated correctly (#180)
    * fix(wait): if exception during waiting occured, the model served_by field was never updated correctly
    
    * fix black and update pre-commit version to be consistent with linter

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 NAME = "superai"
 
-VERSION = "0.1.0.beta5.dev13"
+VERSION = "0.1.0.beta5.dev14"
 # To install the library, run the following
 #
 # python setup.py install
