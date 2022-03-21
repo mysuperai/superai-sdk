76d972acdc5edc94a61d46a818038c799f7169e7
Mirko Bonotto <mirko0x5f@users.noreply.github.com>
2022-03-21 16:58:09 +0100

    feat: compatibility for 3 hero new schema (#185)
    * feat: compatibility for 3 hero new schema
    
    * fix: More clear types, isinstance checks

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 NAME = "superai"
 
-VERSION = "0.1.0.beta5.dev17"
+VERSION = "0.1.0.beta5.dev18"
 # To install the library, run the following
 #
 # python setup.py install
