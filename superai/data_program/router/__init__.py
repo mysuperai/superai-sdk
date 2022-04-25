0ccaa2752fceb06c9351828a4a3d19bbb2cd4581
Lukas Pfannschmidt <lukas@super.ai>
2022-04-25 12:22:17 +0200

    ci: adds earthly file and semgrep rules (#196)
    * feat: add earthfile, unify version reference, add linter target, cache semgrep rules
    
    * exclude .ci folders from manifest
    
    * fix: formatting for all files
    
    * use main branch

--- superai/data_program/router/__init__.py
+++ superai/data_program/router/__init__.py
@@ -1,5 +1,5 @@
 from .filters import standard_filter
 from .router import Router
+from .training import Training
 
 from .basic_router import BasicRouter  # noqa # isort: skip
-from .training import Training
