05265668d524de2020a53dab0b080271eb985959
Mirko Bonotto <mirko0x5f@users.noreply.github.com>
2022-06-16 09:22:13 +0200

    feat: Relax mandatory auth (#235)

--- superai/client.py
+++ superai/client.py
@@ -58,15 +58,15 @@ class Client(
         headers = {}
         if required_api_key:
             if not self.api_key:
-                raise ValueError("API key is required, but not present")
+                logger.error("API key is required, but not present")
             headers["API-KEY"] = self.api_key
         if required_auth_token:
             if not self.auth_token:
-                raise ValueError("auth token is required, but not present")
+                logger.error("AUTH token is required, but not present")
             headers["AUTH-TOKEN"] = self.auth_token
         if required_id_token:
             if not self.id_token:
-                raise ValueError("ID token is required, but not present")
+                logger.error("ID token is required, but not present")
             headers["ID-TOKEN"] = self.id_token
 
         resp = requests.request(
