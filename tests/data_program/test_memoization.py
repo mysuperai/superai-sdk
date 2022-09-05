b2ca39e2ee24f66c534a06fdde8f3b359be1c682
Lukas Pfannschmidt <lukas@super.ai>
2022-09-05 14:38:42 +0200

    perf: improve memo memory footprint and performance for many concurrent threads. (#291)
    * perf: improve memo performance for many concurrent threads.
    
    Lazily inits. the boto3 client and shares it between threads to reduce memory footprint.
    
    * fix test and rename init method

--- tests/data_program/test_memoization.py
+++ tests/data_program/test_memoization.py
@@ -1,7 +1,5 @@
 import os.path
 
-from diskcache import Cache
-
 from superai import settings
 from superai.data_program.experimental import memo, memoization
 
@@ -15,12 +13,14 @@ def test_memo(monkeypatch, tmp_path):
     monkeypatch.setattr(memoization, "_push_to_s3", lambda *args, **kwargs: None)
     monkeypatch.setattr(memoization, "_pull_from_s3", lambda *args, **kwargs: None)
     monkeypatch.setattr(memoization, "_refresh_push_to_s3", lambda *args, **kwargs: None)
+    monkeypatch.setattr(memoization, "cache_settings", mocked_cache_settings)
+    monkeypatch.setattr(memoization, "cache", None)
     filename = "get_job_result/1231"
     filepath = os.path.join("memo", settings.name, filename)
-    mocked_cache = Cache(**mocked_cache_settings)
-    mocked_cache.delete(filepath)
-    assert mocked_cache.get(filepath) is None
-    mocked_cache[filepath] = method()
-    monkeypatch.setattr(memoization, "cache_settings", mocked_cache_settings)
+    memoization._init_cache()
+    cache = memoization.cache
+    cache.delete(filepath)
+    assert cache.get(filepath) is None
+    cache[filepath] = method()
     result = memo(method, filename)
     assert result == method()
