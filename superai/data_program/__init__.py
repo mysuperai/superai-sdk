8e7e2967f089db5aa6eb5cd1c92ddb3df5814f40
Lukas Pfannschmidt <lukas@super.ai>
2022-11-30 15:17:08 +0100

    fix: add missing Workflow import to data_program module (#333)

--- superai/data_program/__init__.py
+++ superai/data_program/__init__.py
@@ -3,6 +3,6 @@ from .types import HandlerOutput  # noqa # isort:skip
 from .data_program import DataProgram, JobContext, PostProcessContext  # noqa # isort:skip
 from .task import Task
 from .task.types import Metric, TaskTemplate, Worker, WorkerType
-from .workflow import WorkflowConfig
+from .workflow import Workflow, WorkflowConfig
 
 from .project import Project  # noqa # isort:skip
