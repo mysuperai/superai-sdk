892e482a6546bafbe9dfd6cccb7f7888e6309c29
fernandodimperio <fernando.dimperio@gmail.com>
2023-11-22 14:51:11 -0300

    patch jobs download_jobs method
    added batch_id and search_id as parameters

--- superai/apis/jobs.py
+++ superai/apis/jobs.py
@@ -188,6 +188,9 @@ class JobsApiMixin(ABC):
         status_in: List[str] = None,
         send_email: bool = None,
         with_history: bool = None,
+        #fernando
+        batch_id: str = None,
+        id_search: str = None,
     ) -> dict:
         """
         Trigger processing of jobs responses that are sent to customer email (default) once is finished.
@@ -221,6 +224,10 @@ class JobsApiMixin(ABC):
             query_params["sendEmail"] = send_email
         if with_history is not None:
             query_params["withHistory"] = with_history
+        if batch_id is not None:
+            query_params["batchId"] = batch_id
+        if id_search is not None:
+            query_params["idSearch"] = id_search
         return self.request(uri, method="POST", query_params=query_params, required_api_key=True)
 
     def get_all_jobs(
