8ca9221e675e522a4a282c3968aa9154775326ed
fernandodimperio <fernando.dimperio@gmail.com>
2023-11-22 14:14:33 -0300

    black formatter formatting

--- superai/apis/jobs.py
+++ superai/apis/jobs.py
@@ -147,8 +147,10 @@ class JobsApiMixin(ABC):
         body_json = {}
         if tags is not None:
             body_json["tags"] = tags
-        return self.request(uri, method="DELETE", query_params=query_params,body_params=body_json, required_api_key=True)
-        
+        return self.request(
+            uri, method="DELETE", query_params=query_params, body_params=body_json, required_api_key=True
+        )
+
     def list_jobs(
         self,
         app_id: str,
